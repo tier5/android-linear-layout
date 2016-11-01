8c6e8a9031740269412d645e77fe997f566caf05
tier511 <work@tier5.us>
2016-11-01 20:30:01 +0530

    Linear Layout v1.0

---
+++ app/src/main/java/us/tier5/notebook3/MainActivity.java
@@ -0,0 +1,29 @@
+package us.tier5.notebook3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.list_itms);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "fuck you"+i,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
